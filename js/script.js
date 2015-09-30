0fdb7df8794b830c7197a64db4aa002e56bf688c
PaulRossDavidson <paulrossdavidson@gmail.com>
2015-09-29 19:30:40 -0700

    scrolling.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log("script.js is linked")
+$(document).ready(function() { 
+
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+$("#about").click(function() {
+   scrollToAnchor('about');
+});
+$("#experience").click(function() {
+   scrollToAnchor('experience');
+});
+$("#projects").click(function() {
+   scrollToAnchor('projects');
+});
+$("#contact").click(function() {
+   scrollToAnchor('contact');
+});
+});
